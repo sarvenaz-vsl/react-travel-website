bd0baf183cae4cccf8680725bc68634b841e1a89
sarvenaz-vsl <sarvenaz.vsl@gmail.com>
2021-06-04 12:12:50 +0430

    Little Changes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' component={Home} />
+          <Route path='/' exact component={Home} />
           <Route path='/services' component={Services } />
           <Route path='/products' component={Products} />
           <Route path='/sign-up' component={SignUp} />
